83870ae0031c28b72d8676963b3e6b0384dceb52
amine bajji <aminebajji34@gmail.com>
2024-08-16 22:11:49 -0700

    Full project UPDATE back-end & front-end while adding PostgreSQL database to make the data persistant

--- index.js
+++ index.js
@@ -1,41 +1,75 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 
 const app = express();
 const port = 3000;
 
 
+const db = new pg.Client ({
+   user: "postgres",
+   host: "localhost",
+   database: "todolist",
+   password: "mydb123456",
+   port: 5432,
+});
+
+db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 
-let itemList = [];
+let items = [];
 
-// Add item to itemList array
-function addItemToArray(newItem, itemId) {
-   itemList.push({itemId, newItem});
-   console.log(itemList);
-}
-
-app.post('/', (req, res) => {
-   const newItem = req.body["newItem"];
-   const itemId = String(Date.now());
-   addItemToArray(newItem, itemId);
-   res.redirect('/');
-}); 
-
-// get item ids for deletion
-app.post('/delete', (req, res) => {
-   const itemId = req.body.itemId;
-   itemList = itemList.filter(item => item.itemId !== itemId);
-   console.log(itemList);
-   res.sendStatus(200);
+// Add items to List array
+app.post("/add", async (req, res) => {
+   const item = req.body.newItem;
+   try {
+     await db.query("INSERT INTO items_list (title) VALUES ($1)", [item]);
+     res.redirect("/");
+   } catch (err) {
+     console.log(err);
+   }
  });
 
-app.get('/',(req, res) => {
-   res.render("index.ejs", { toDoList: itemList });
-});
+// delete items from db
+app.post("/delete", async (req, res) => {
+   const id = req.body.deleteItemId;
+   try {
+     await db.query("DELETE FROM items_list WHERE id = $1", [id]);
+     res.redirect("/");
+   } catch (err) {
+     console.log(err);
+   }
+ });
+
+ // edit items on display and simultaneously on db
+ app.post("/edit", async (req, res) => {
+   const item = req.body.updatedItemTitle;
+   const id = req.body.updatedItemId;
+ 
+   try {
+     await db.query("UPDATE items_list SET title = ($1) WHERE id = $2", [item, id]);
+     res.redirect("/");
+   } catch (err) {
+     console.log(err);
+   }
+ });
+
+ app.get("/", async (req, res) => {
+   try {
+     const result = await db.query("SELECT * FROM items_list ORDER BY id ASC");
+     items = result.rows;
+     res.render("index.ejs", {
+       listTitle: "Today",
+       listItems: items,
+     });
+   } catch (err) {
+     console.log(err);
+   }
+ });
 
 app.get("/code-rev", (req, res) => {
    res.render("explanations.ejs");
