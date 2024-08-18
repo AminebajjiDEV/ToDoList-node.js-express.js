0c0140218dd6271a18ff7ad5907f138c9dad50e2
amine bajji <aminebajji34@gmail.com>
2024-08-17 18:24:53 -0700

    updated styling

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import dotenv from "dotenv";
 
 
+dotenv.config();
 const app = express();
 const port = 3000;
 
 
-const db = new pg.Client ({
-   user: "postgres",
-   host: "localhost",
-   database: "todolist",
-   password: "mydb123456",
-   port: 5432,
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 db.connect();
